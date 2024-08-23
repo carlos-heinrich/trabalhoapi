8999440a6ce83bd1eae83f9312e915f66dd5e54b
steffandrade <stefanyandrade.gomes@gmail.com>
2024-08-23 11:15:38 -0300

    correção na rota api

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Controllers\SportController;
+use App\Http\Controllers\TrainerController;
+use App\Http\Controllers\CompetitorController;
+use App\Http\Controllers\LocalityController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,3 +19,6 @@ Route::get('/me', function () {
 });
 
 Route::apiResource('/sports', SportController::class);
+Route::apiResource('/trainer', TrainerController::class);
+Route::apiResource('/competitor', CompetitorController::class);
+Route::apiResource('/locality', LocalityController::class);
